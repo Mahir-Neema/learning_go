c65ea9cb76ab6f4342ba472ba455fa393e3540d2
Mahir-Neema <mohi10503@gmail.com>
2023-10-22 22:20:12 +0530

    Add a course controller in golang

--- 24buildapi/main.go
+++ 24buildapi/main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"math/rand"
 	"net/http"
+	"strconv"
+	"time"
 
 	"github.com/gorilla/mux"
 )
@@ -25,7 +28,8 @@ var courses []Course
 
 // middleware, helper - file
 func (c *Course) IsEmpty() bool {
-	return c.CourseId == "" && c.CourseName == ""
+	// return c.CourseId == "" && c.CourseName == ""
+	return c.CourseName == ""
 }
 
 func main() {
@@ -53,12 +57,38 @@ func getOneCourse(w http.ResponseWriter, r *http.Request) {
 
 	//lop through the courses and find the matching id
 	for _, course := range courses {
-		if course.courseId == params["id"] {
+		if course.CourseId == params["id"] {
 			json.NewEncoder(w).Encode(course)
 			return
 		}
 	}
 
-	json.NewEncoder(w).Encode("Noe course found with the given id")
+	json.NewEncoder(w).Encode("No course found with the given id")
 	return
+}
+
+func CreateOneCourse(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("create one course")
+	w.Header().Set("Content-Type", "application/json")
+
+	// what if body is empty?
+	if r.Body == nil {
+		json.NewEncoder(w).Encode("please send some data")
+	}
+	// what about - {}
+	var course Course
+	_ = json.NewDecoder(r.Body).Decode(&course)
+
+	if course.IsEmpty() {
+		json.NewEncoder(w).Encode("please send some data")
+		return
+	}
+
+	// random id int string
+	rand.Seed(time.Now().UnixNano())
+	course.CourseId = strconv.Itoa(rand.Intn(100))
+	courses = append(courses, course)
+	json.NewEncoder(w).Encode(course)
+	return
+
 }
